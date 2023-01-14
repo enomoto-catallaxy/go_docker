15ae7df5ccb99c277aa82544ca93e5b4cce72796
enomoto-catallaxy <t.enomoto@catallaxy.me>
2023-01-15 00:46:53 +0900

    make backend server

---
+++ golang/src/go.mod
@@ -0,0 +1,7 @@
+module go_docker
+
+go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
+
+replace go_docker/article => ./path/to/local/package
---
+++ golang/src/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func open(path string, count uint) *sql.DB {
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		log.Fatal("open error:", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		time.Sleep(time.Second * 2)
+		count--
+		fmt.Printf("retry... count:%v\n", count)
+		return open(path, count)
+	}
+
+	fmt.Println("db connected!!")
+	return db
+}
+
+func connectDB() *sql.DB {
+	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
+		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"))
+
+	return open(path, 100)
+}
+
+func main() {
+	db := connectDB()
+	defer db.Close()
+	ReadAll(db)
+}
+
+//----------------------------------------------------------------------------------
+// TODO: ./article/article.goからimportする
+
+type Article struct {
+	id    int
+	title string
+	body  string
+}
+
+func ReadAll(db *sql.DB) {
+	var articles []Article
+	rows, err := db.Query("select * from article;")
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		article := Article{}
+		err = rows.Scan(&article.id, &article.title, &article.body)
+		if err != nil {
+			panic(err)
+		}
+		articles = append(articles, article)
+	}
+	rows.Close()
+
+	fmt.Println(articles)
+}
