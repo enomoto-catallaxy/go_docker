d25668766f645e7017c457caf3f9e96c2e57447f
enomoto-catallaxy <t.enomoto@catallaxy.me>
2023-01-15 19:31:50 +0900

    minimize main.go

--- golang/src/main.go
+++ golang/src/main.go
@@ -1,57 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"time"
+	"go_docker/routes"
 
-	"go_docker/article"
-
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func open(path string, count uint) *sql.DB {
-	db, err := sql.Open("mysql", path)
-	if err != nil {
-		log.Fatal("open error:", err)
-	}
-
-	if err = db.Ping(); err != nil {
-		time.Sleep(time.Second * 2)
-		count--
-		fmt.Printf("retry... count:%v\n", count)
-		return open(path, count)
-	}
-
-	fmt.Println("db connected!!")
-	return db
-}
-
-func connectDB() *sql.DB {
-	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
-		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_DATABASE"))
-
-	return open(path, 100)
-}
-
-func runGin() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
 func main() {
-	db := connectDB()
+	db := routes.ConnectDB()
 	defer db.Close()
-	article.ReadAll(db)
-	runGin()
+	routes.RunGin(db)
 }
