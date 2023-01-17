fd66adcce89e6c8892adb394e48cdab55ca12166
enomoto-catallaxy <t.enomoto@catallaxy.me>
2023-01-17 21:56:59 +0900

    make database.go

---
+++ golang/src/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"go_docker/common"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	var path string = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true",
+		os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"))
+
+	db := open(path)
+
+	common.Migrate(db)
+
+	return db
+}
+
+func open(path string) *gorm.DB {
+	sqlDB, err := sql.Open("mysql", path)
+
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("open error:", err)
+	}
+
+	fmt.Println("db connected!!")
+	return db
+}
