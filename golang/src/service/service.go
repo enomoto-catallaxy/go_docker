add85e852816ec4719a4f0dce86ab4873c1ea24e
enomoto-catallaxy <t.enomoto@catallaxy.me>
2023-01-28 16:37:08 +0900

    空行を削除

--- golang/src/service/service.go
+++ golang/src/service/service.go
@@ -9,24 +9,18 @@ import (
 
 func GetArticles(db *gorm.DB) []entity.Article {
 	var articles []entity.Article
-
 	db.Find(&articles).Scan(&articles)
-
 	return articles
 }
 
 func GetArticle(db *gorm.DB, id gin.Params) entity.Article {
 	var article entity.Article
-
 	db.Table("articles").Where("id = ?", id[0].Value).Scan(&article)
-
 	return article
 }
 
 func GetSeatById(db *gorm.DB, id gin.Params) entity.Seat {
 	var seat entity.Seat
-
 	db.Table("seats").Where("seat_id = ?", id[0].Value).Scan(&seat)
-
 	return seat
 }
