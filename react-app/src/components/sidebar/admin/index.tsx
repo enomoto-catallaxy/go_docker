751a3d0c663764d91c4ebbe2e289dc65aec9c14e
enomoto-catallaxy <t.enomoto@catallaxy.me>
2023-01-31 03:00:53 +0900

    adminアイコンの中身作成

---
+++ react-app/src/components/sidebar/admin/index.tsx
@@ -0,0 +1,55 @@
+import { DiAptana } from "react-icons/di";
+import { memo } from "react";
+import styled from "styled-components";
+import { Popover } from "antd";
+import { BiUserPlus } from "react-icons/bi";
+import { AiOutlineFileSearch } from "react-icons/ai";
+
+export const SideBarAdmin = memo(() => {
+  const handleToRegiserForm = () => {
+    window.location.href = "/new/student";
+  };
+
+  const content = (
+    <>
+      <Icons onClick={handleToRegiserForm}>
+        <BiUserPlus size={48}></BiUserPlus>
+      </Icons>
+      <Icons onClick={handleToRegiserForm}>
+        <AiOutlineFileSearch size={48}></AiOutlineFileSearch>
+      </Icons>
+    </>
+  );
+
+  return (
+    <Wrapper>
+      <Popover placement="right" content={content} trigger="hover">
+        <DiAptana size={48}></DiAptana>
+      </Popover>
+    </Wrapper>
+  );
+});
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 48px;
+  /* padding-left: -6px;
+  padding-right: -6px; */
+  margin-bottom: 24px;
+  display: flex;
+  justify-content: space-evenly;
+
+  :hover {
+    cursor: pointer;
+    color: blue;
+  }
+`;
+
+const Icons = styled.div`
+  margin-bottom: 10%;
+
+  :hover {
+    cursor: pointer;
+    color: blue;
+  }
+`;
